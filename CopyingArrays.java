dfd841a3d8d986d5df2815b5e0dc770e140ecfd7
João <40552750+Joao-Regis@users.noreply.github.com>
2019-09-25 23:53:53 -0700

    Add files via upload

---
+++ CopyingArrays.java
@@ -0,0 +1,28 @@
+/**
+ * CopyingArrays
+ */
+public class CopyingArrays {
+
+    public static void main(String[] args) {
+        
+        int[] firstArray = {5, 10, 15, 20, 25};
+        int[] secondArray = new int[5];
+
+        for (int index = 0; index < firstArray.length; index++)
+        {
+            secondArray[index] = firstArray[index];
+        }
+        System.out.println();
+
+        for(int index = 0; index < firstArray.length; index++)
+            {
+                System.out.print(firstArray[index] + ' ');
+            }
+        System.out.println();
+        
+        for(int index = 0; index < secondArray.length; index++)
+        {
+            System.out.print(secondArray[index] + ' ');
+        }
+    }
+}
