a595437e83b87ecf1263c96759a5f8952f1a0ab0
João <40552750+Joao-Regis@users.noreply.github.com>
2019-09-28 07:54:06 -0700

    Update S_mallestA-rray_I_tem.java

--- S_mallestA-rray_I_tem.java
+++ S_mallestA-rray_I_tem.java
@@ -1,10 +1,61 @@
+/**
+ * SmallestArrayItem
+ */
+
+import java.util.Scanner;
 
 public class SmallestArrayItem {
 
-	public static void main(String[] args) {
-		
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        
+        int arrSize = 5;
+        boolean matchFound = false;
 
-	}
+        int [] arrOne = new int[arrSize];
+        int [] arrTwo = new int[arrSize];
 
+        System.out.println("Please enter " +
+            arrSize + " values for array 1");
+        for(int i = 0; i <arrOne.length; i++)
+        {
+            arrOne[i] = keyboard.nextInt();
+        }
+        
+        System.out.println("Please enter " +
+            arrSize + " values for array 2");
+        for(int i = 0; i <arrTwo.length; i++)
+        {
+            arrTwo[i] = keyboard.nextInt();
+        }
+        
+
+
+        int commonInt = 0;
+        for(int i = 0; i < arrOne.length; i++)
+            {
+                for(int num = 0; num < arrTwo.length; num++)
+                {
+                    if(arrOne[i] == arrTwo[num])
+                    {
+                        matchFound = true;
+                        commonInt = arrOne[i];
+                    }
+                }
+            }
+            
+        
+        if(matchFound)
+        {
+            System.out.println("The Smallest match " +
+                        "in the array is : " + 
+                        commonInt);
+        }
+        else
+        {
+            System.out.println("There is no smallest matching iteger!");
+        }
+
+        keyboard.close();
+    }
 }
-
