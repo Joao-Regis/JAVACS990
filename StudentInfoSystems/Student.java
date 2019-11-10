2efc613f5e69d75bc2f3e77f3af3d5d2d1724db9
João <40552750+Joao-Regis@users.noreply.github.com>
2019-11-09 20:38:36 -0800

    Update Student.java

--- StudentInfoSystems/Student.java
+++ StudentInfoSystems/Student.java
@@ -3,64 +3,99 @@
  */
 public class Student 
 {
-    static int stid;
-    
-    String name;
-    String address;
-    String city;
-    String state;
+    public static int studentCount = 0;
+    int stid;// = studentCount; // 01 int = 04 bytes
+    String name;    // 20 chars = 40 bytes
+    String address; // 20 chars = 40 bytes
+    String city;    // 20 chars = 40 bytes
+    String state;   // 02 chars = 04 bytes -- Total 128 bytes
 
-    public Student(String n, String a, String c, String s) {
+    public Student( String n, String a, String c, String s) 
+    {
+        //increase stid by one. unique key value
+        //so that next student has unique stid.
+        studentCount++;
+        
         this.name = n;
         this.address = a;
         this.city = c;
         this.state = s;
+        this.stid = studentCount;
+
+        
+    }
+
+    
+
+    public Student(int i, String n, String a, String c, String s) 
+    {
+        this.stid = i;
+        this.name = n;
+        this.address = a;
+        this.city = c;
+        this.state = s;        
     }
 
 
-    public static int getStid() {
+    public int getStudentCount()
+    {
+        return studentCount;
+    }
+
+    public int getStid() 
+    {
         return stid;
     }
 
-    public static void setStid(int stid) {
-        Student.stid = stid;
+    public void setStid(int stid) 
+    {
+        this.stid = stid; // changed from student.stid
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
+    public void setName(String name) 
+    {
         this.name = name;
     }
 
-    public String getAddress() {
+    public String getAddress() 
+    {
         return address;
     }
 
-    public void setAddress(String address) {
+    public void setAddress(String address) 
+    {
         this.address = address;
     }
 
-    public String getCity() {
+    public String getCity() 
+    {
         return city;
     }
 
-    public void setCity(String city) {
+    public void setCity(String city) 
+    {
         this.city = city;
     }
 
-    public String getState() {
+    public String getState() 
+    {
         return state;
     }
 
-    public void setState(String state) {
+    public void setState(String state) 
+    {
         this.state = state;
     }
 
     @Override
-    public String toString() {
-        return "Student [address=" + address + ", city=" + city + ", name=" + name + ", state=" + state + "]";
+    public String toString() 
+    {
+        return "Student [address=" + address + ", city=" + city + 
+                        ", name=" + name + ", state=" + state + "]";
     }
 
     
