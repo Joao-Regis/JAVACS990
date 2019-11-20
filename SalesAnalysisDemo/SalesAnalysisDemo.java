9b34a043968e261466a2e192b0d1e5c6f1de5825
João <40552750+Joao-Regis@users.noreply.github.com>
2019-11-20 14:33:56 -0800

    Update SalesAnalysisDemo.java

--- SalesAnalysisDemo/SalesAnalysisDemo.java
+++ SalesAnalysisDemo/SalesAnalysisDemo.java
@@ -8,7 +8,26 @@ public class SalesAnalysisDemo
 {
 
     public static void main(String[] args) 
+                        throws FileNotFoundException 
     {
+     
+        double totalSales = 0;
+        String salesFigures;
+        double avg;
+        double salesForWeek;
+        double highSales;
+        int highSalesWeek;
+        double lowSales;
+        int lowSalesWeek;
+
+        File salesDataFile = new File("SalesData.txt");
+        Scanner inputFile = new Scanner(salesDataFile);
+
+        while(inputFile.hasNext())
+        {
+            
+        }
+
         
     }
 }
