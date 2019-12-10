d7bda36d41d21760040f3c761983a5d02db6d785
João <40552750+Joao-Regis@users.noreply.github.com>
2019-12-10 14:17:18 -0800

    Create EnrollmentRecordsFileManager.java

---
+++ Final/EnrollmentRecordsFileManager.java
@@ -0,0 +1,241 @@
+package sample;
+
+import java.io.*;
+
+/**
+ * EnrollmentRecordsFileManager
+ * This class manages a random access file which contains
+*  Enrollment records.
+ */
+public class EnrollmentRecordsFileManager 
+{
+    public final int RECORD_SIZE = 30;
+    private RandomAccessFile enrollmentFile; 
+
+    /**
+     The constructor opens a random access file
+    for both reading and writing.
+    @param filename The name of the file.
+    @exception FileNotFoundException When the file
+                is not found.
+    */
+
+    public EnrollmentRecordsFileManager(String filename)
+                        throws FileNotFoundException
+    {
+        // Open the file for reading and writing.
+        enrollmentFile = new RandomAccessFile(filename, "rw");
+    }
+
+
+    public void CreateEnrollmentFile(Enrollment aEnrollment) throws IOException
+    {
+
+        // move file pointer to eof to append new enrollment.
+        moveFilePointer(getNumberOfRecords());
+
+        writeEnrollment(aEnrollment);
+    }
+
+
+
+    /**
+     The writeEnrollment method writes the contents
+    of a Enrollment objects to the file at the
+    current file pointer position.
+    @param enrollment The Enrollment objects to write.
+    @exception IOException When a file error occurs.
+    */
+
+
+    public void writeEnrollment(Enrollment enrollment)
+                                throws IOException
+    {
+        // Write the eSID to the file.
+        enrollmentFile.writeInt(enrollment.geteSID());
+
+
+        // Write the eCnum to the file.
+        enrollmentFile.writeInt(enrollment.geteCNum());
+
+    
+        // Get the enrollment's YEAR.
+        enrollmentFile.writeInt(enrollment.getYear());
+
+    
+        // Get the enrollment's SEMESTER.
+        String str = enrollment.getSemester();
+
+        // Write the Semester.
+        if (str.length() > 6)
+        {
+            // If there are more than 6 characters in the
+            // string, then write only the first 6.
+            for (int i = 0; i < 6; i++)
+            enrollmentFile.writeChar(str.charAt(i));
+        }
+        else
+        {
+            // Write the semester to the file.
+            enrollmentFile.writeChars(str);
+            // Write enough spaces to pad it out
+            // to 6 characters.
+            for (int i = 0; i < (6 - str.length()); i++)
+            enrollmentFile.writeChar(' ');
+        }
+
+
+        // Get the enrollment's GRADE.
+        enrollmentFile.writeChar(enrollment.getGrade());
+
+
+        // Write the eSID to the file.
+        enrollmentFile.writeInt(enrollment.getEnrollmentID());
+
+        
+        System.out.println("The data was written to the " +
+                            "Enrollments.dat file.");
+    }
+
+
+
+
+
+
+    public Enrollment ReadEnrollmentFile(int enrollmentNumID) throws IOException
+    {
+        int recordNumber = enrollmentNumID;     // Record number
+        Enrollment enrollment;   // An object from the file
+
+        // Report the number of records in the file.
+        System.out.println("The Enrollments.dat file has " +
+                getNumberOfRecords() + " records.");
+
+        // Move the file pointer to that record.
+        moveFilePointer(recordNumber - 1);
+
+        // Read the record at that location.
+        enrollment = readEnrollment();
+
+        // Display the record.
+        System.out.println("Student ID: " +
+                enrollment.geteSID());
+        System.out.println("CNum: " + enrollment.geteCNum());
+        System.out.println("Year: " +
+                enrollment.getYear());
+        System.out.println("Semester: " +
+                enrollment.getSemester());
+        System.out.println("Grade: " +
+                enrollment.getGrade());
+        System.out.println("Enrollment ID: " +
+                enrollment.getEnrollmentID());
+
+        return enrollment;
+
+    }
+
+
+
+
+
+
+    /**
+        The readEnrollment method reads and returns
+        the record at the current file pointer position.
+        @return A reference to a Enrollment objects.
+        @exception IOException When a file error occurs.
+    */
+
+    public Enrollment readEnrollment() throws IOException
+    {
+        
+        // Read the STID from the file.
+        int eSTID = enrollmentFile.readInt();
+        
+        // Read the CNUM from the file.
+        int eCNUM = enrollmentFile.readInt();
+        
+        // Read the YEAR from the file.
+        int eYEAR = enrollmentFile.readInt();
+
+
+        char[] charArray = new char[6];
+        
+        // Read the Semester, character by character,
+        // from the file into the char array.
+        for (int i = 0; i < 6; i++)
+            charArray[i] = enrollmentFile.readChar();
+            
+        // Store the char array in a String.
+        String eSEM = new String(charArray);
+        
+        // Trim any trailing spaces from the string.
+        eSEM.trim();
+
+
+        // Read the YEAR from the file.
+        char eGRADE = enrollmentFile.readChar();
+
+
+        // Read the cnum from the file.
+        int eID = enrollmentFile.readInt();
+
+    
+        // Create a Enrollment objects and initialize
+        // it with these values.
+        Enrollment enrollment =
+                new Enrollment(eSTID, eCNUM, eYEAR, eSEM, eGRADE, eID);
+        
+        // Return the object.
+        return enrollment;
+    }
+
+    
+    /**
+        The getByteNum method returns a record's
+        starting byte number.
+        @param recordNum The record number of the
+                        desired record.
+    */
+
+    private long getByteNum(long recordNum)
+    {
+        return RECORD_SIZE * recordNum;
+    }
+
+
+    /**
+        The moveFilePointer method moves the file
+        pointer to a specified record.
+        @param recordNum The number of the record to move to.
+        @exception IOException When a file error occurs.
+    */
+
+    public void moveFilePointer(long recordNum)
+                            throws IOException
+    {
+        enrollmentFile.seek(getByteNum(recordNum));
+    }
+
+    /**
+        The getNumberOfRecords method returns the number
+        of records stored in the file.
+        @return The number of records in the file.
+        @exception IOException When a file error occurs.
+    */
+
+    public long getNumberOfRecords() throws IOException
+    {
+        return enrollmentFile.length() / RECORD_SIZE;
+    }
+
+    /**
+        The close method closes the file.
+        @exception IOException When a file error occurs.
+    */
+
+    public void close() throws IOException
+    {
+        enrollmentFile.close();
+    }
+}
