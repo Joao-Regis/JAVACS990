b3ac055f9b7c1902b6bc579d5238b71c770cf1d7
João <40552750+Joao-Regis@users.noreply.github.com>
2019-12-09 22:48:23 -0800

    Create Student.java

---
+++ Final/Student.java
@@ -0,0 +1,109 @@
+package sample;
+
+import java.io.IOException;
+
+/**
+ * Student
+ */
+public class Student
+{
+
+    //!!! Add command to keep track of studentCount after reset
+
+    public static int studentCount = 0;
+    int stid;// = studentCount; // 01 int = 04 bytes
+    String name;    // 20 chars = 40 bytes
+    String address; // 20 chars = 40 bytes
+    String city;    // 20 chars = 40 bytes
+    String state;   // 02 chars = 04 bytes -- Total 128 bytes
+
+    public Student( String n, String a, String c, String s, StudentRecordsFileManager sfilemngr) throws IOException {
+        //increase studentCount by one. unique key value
+        //so that next student has unique stid.
+        studentCount++;
+        
+        this.name = n;
+        this.address = a;
+        this.city = c;
+        this.state = s;
+        this.stid = ((int) sfilemngr.getNumberOfRecords()) + 1;
+
+
+        
+    }
+
+    
+
+    public Student(int i, String n, String a, String c, String s) 
+    {
+        this.stid = i;
+        this.name = n;
+        this.address = a;
+        this.city = c;
+        this.state = s;        
+    }
+
+
+    public int getStudentCount()
+    {
+        return studentCount;
+    }
+
+    public int getStid() 
+    {
+        return stid;
+    }
+
+    public void setStid(int stid) 
+    {
+        this.stid = stid; // changed from student.stid
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) 
+    {
+        this.name = name;
+    }
+
+    public String getAddress() 
+    {
+        return address;
+    }
+
+    public void setAddress(String address) 
+    {
+        this.address = address;
+    }
+
+    public String getCity() 
+    {
+        return city;
+    }
+
+    public void setCity(String city) 
+    {
+        this.city = city;
+    }
+
+    public String getState() 
+    {
+        return state;
+    }
+
+    public void setState(String state) 
+    {
+        this.state = state;
+    }
+
+    @Override
+    public String toString() {
+        return "Student [address=" + address + ", city=" + city + ", name=" + name + ", state=" + state + ", stid="
+                + stid + "]";
+    }
+
+    
+    
+}
